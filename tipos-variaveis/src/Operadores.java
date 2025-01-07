d5a0dad618fda00cdaff872b6d44c0088b29d484
Victor Hugoe <dev.victor011@gmail.com>
2025-01-07 14:16:53 -0300

    tipos de variaveis e operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,77 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2){
+            System.out.println("as duas condicoes sao verdadeiras");
+        }
+
+        if (condicao1 || condicao2){
+            System.out.println("Uma das condicoes sao verdadeiras");
+        }
+
+        if (condicao1 && 7 > 4){
+            System.out.println("E");
+        }
+
+
+
+//        int numero1 = 1;
+//        int numero2 = 2;
+//
+//        boolean simNao = numero1 == numero2;
+//
+//        System.out.println("NumeroUm é ihual a numeroDois ? " + simNao);
+//
+//        simNao = numero1 != numero2;
+//
+//        System.out.println("NumeroUm é diferente do numeroDois ? " + simNao);
+//
+//        simNao = numero1 > numero2;
+//
+//        System.out.println("NumeroUm é maior que numeroDois ? " + simNao);
+//
+//        simNao = numero1 >= numero2;
+//
+//        System.out.println("NumeroUm é maior ou = que numeroDois ? " + simNao);
+//
+//        simNao = numero1 < numero2;
+//
+//        System.out.println("NumeroUm é menor que numeroDois ? " + simNao);
+//
+//        simNao = numero1 <= numero2;
+//
+//        System.out.println("NumeroUm é menor ou igual que numeroDois ? " + simNao);
+
+        //int a, b;
+
+        //a = 6;
+        //b = 6;
+
+        //String resultado = a==b ? "verdadeiro" : "falso";
+
+        //System.out.println(resultado);
+
+        //numero = - numero;
+        //System.out.println(numero);
+        //numero = numero * -1;
+        //System.out.println(numero);
+
+        //x repeticao
+        //numero++;
+        //numero = numero +1;
+
+        //System.out.println(++ numero);
+        //System.out.println(numero);
+
+        //boolean variavel = true;
+
+        //variavel = !variavel;
+
+        //System.out.println(variavel);
+
+    }
+
+}
