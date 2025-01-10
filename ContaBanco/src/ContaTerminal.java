d1b5ad9bffc4244320ab65d4c2a7f254dbaa606b
Victor Hugoe <dev.victor011@gmail.com>
2025-01-09 23:31:41 -0300

    Criação do projeto ContaBanco com entrada de dados via terminal e exibição de mensagem formatada.

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        //TOD0: conhecer e importar a classe scanner
+        //Exibir as mensagens para o usuario
+        //Obter pelo scanner os valores digitados no terminal
+        //Exibir a mensagem conta criada
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o seu nome:");
+        scanner.nextLine();
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo inicial:");
+        double saldo = scanner.nextDouble();
+
+        //saida formatada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + numeroConta + " é seu saldo " + saldo + " já está disponível para saque.");
+
+        //fecha scanner
+        scanner.close();
+
     }
 }
