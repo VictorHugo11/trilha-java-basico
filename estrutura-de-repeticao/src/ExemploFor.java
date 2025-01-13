994b24bdf34cbacc68792b1e77acfd827d90dd17
Victor Hugoe <dev.victor011@gmail.com>
2025-01-13 16:47:51 -0300

    estrutura de repeticao

---
+++ estrutura-de-repeticao/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+
+        //estrutura 1
+        for(int carneirinhos = 1; carneirinhos <= 20;  carneirinhos ++){
+            System.out.println(carneirinhos + " - Carneirinho(s)");
+        }
+       //estrutura 2
+       //o que importa é somente o bloco condicional
