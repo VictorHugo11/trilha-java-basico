5b400ccc9acd42591c9ce3da8d73085c47528ba0
Victor Hugoe <dev.victor011@gmail.com>
2025-03-10 21:38:05 -0300

    pacotes e visibilidades dos recursos

---
+++ poo-java/exemplo-pacotes/src/lanchonete/atendimento/cozinha/Cozinheiro.java
@@ -0,0 +1,53 @@
+package lanchonete.atendimento.cozinha;
+
+import lanchonete.atendimento.Atendente;
+
+public class Cozinheiro {
+    //pode ser default
+    public void adicionarLancheNoBalcao() {
+        System.out.println("ADICIONADO LANCHE NATURAL HAMBURGUER NO BALCAO");
+    }
+    //pode ser default
+    public void adicionarSucoNoBalcao() {
+        System.out.println("ADICIONANDO SUCO NO BALCAO");
+    }
+    //pode ser default
+    public void adicionarComboNoBalcao() {
+        adicionarLancheNoBalcao();
+        adicionarSucoNoBalcao();
+    }
+    private void prepararLanche() {
+        System.out.println("PREPARANDO LANCHE TIPO HAMBURGUER");
+    }
+    private void prepararVitamina() {
+        System.out.println("PREPARANDO SUCO");
+    }
+    private void prepararCombo() {
+        prepararLanche();
+        prepararVitamina();
+    }
+    private void selecionarIngredientesLanche() {
+        System.out.println("SELECIONADO O PÃO, SALADA, OVO E CARNE");
+    }
+    private void selecionarIngredientesSuco() {
+        System.out.println("SELECIONADO FRUTA, LEITE E SUCO");
+    }
+    private void lavarIngredientes() {
+        System.out.println("LAVANDO INGREDIENTES");
+    }
+    private void selecionarIngredientesvitamina() {
+        System.out.println("SELECIONANDO INGREDIENTES VITAMINA");
+    }
+    private void baterVitaminaLiquidificador() {
+        System.out.println("BATENDO VITAMINA LIQUIDIFICADOR");
+    }
+    private void fritarIngredientesLanche() {
+        System.out.println("FRITANDO A CARNE E OVO PARA O HAMBURGUER");
+    }
+    private void pedirParaTrocarGas(Almoxarife meuAmigo) {
+        meuAmigo.trocarGas();
+    }
+    private void pedirIngredientes(Almoxarife almoxarife) {
+        almoxarife.entregarIngredientes();
+    }
+}
