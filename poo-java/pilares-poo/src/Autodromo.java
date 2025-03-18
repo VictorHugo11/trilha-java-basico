1f01c0d92272f450dea24f3e2f46f65c35562d23
Victor Hugoe <dev.victor011@gmail.com>
2025-03-17 21:13:02 -0300

    Pilares de o POO

---
+++ poo-java/pilares-poo/src/Autodromo.java
@@ -0,0 +1,12 @@
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro bmw = new Carro();
+        bmw.setChassi("2312324");
+        bmw.ligar();
+
+        Moto s1000rr = new Moto();
+        s1000rr.setChassi("3412321");
+        s1000rr.ligar();
+        
+    }
+}
